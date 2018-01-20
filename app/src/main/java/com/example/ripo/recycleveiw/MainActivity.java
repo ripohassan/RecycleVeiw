1ce97bbf8c3c2f56772a884c80a683a5973469cc
ripo <ripohassan5@gmail.com>
2018-01-20 11:39:58 +0600

    botton by ripohassan

--- app/src/main/java/com/example/ripo/recycleveiw/MainActivity.java
+++ app/src/main/java/com/example/ripo/recycleveiw/MainActivity.java
@@ -1,24 +1,20 @@
 package com.example.ripo.recycleveiw;
 
-import android.accounts.OnAccountsUpdateListener;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+Button b ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.r);
 
 
-
-
-    }
-
-    @Override
-    public void applyOverrideConfiguration(Configuration overrideConfiguration) {
 
     }
 }
